4717b5119004c424199c44394ceae2c9fac5856b
Theofanis kouniakis <62156040+KouniakiosKatakthths@users.noreply.github.com>
2026-05-07 22:23:56 +0300

    Use user form

--- CloudComputing.Client/src/pages/login.tsx
+++ CloudComputing.Client/src/pages/login.tsx
@@ -3,10 +3,9 @@ import { useAuth } from "../auth/AuthService"
 import { useNavigate } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import ErrorDisplay, { type ApiError } from "../components/ErrorDisplay"
+import UserForm from "../forms/UserForm"
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const [apiError, setApiError] = useState<ApiError | null>(null);
   const { login, isLoggedIn } = useAuth();
   const navigate = useNavigate();
@@ -16,8 +15,7 @@ export default function Login() {
     if (isLoggedIn) navigate('/')
   }, [isLoggedIn])
 
-  const handleLogin = async (e: React.SubmitEvent) => {
-    e.preventDefault();
+  const handleLogin = async (username: string, password: string) => {
     setApiError(null);
 
     const { success, error } = await login(username, password)
@@ -36,12 +34,8 @@ export default function Login() {
         } 
         <div className="container">
           <h2>Καλωσορίσατε</h2>
-          <p>Εισάγετε τα στοιχεία σας για να συνεχίσετε</p>
-          <form className="user-form" style={{ "marginTop": "20px" }} onSubmit={e => handleLogin(e)}>
-            <input className="user-form-field" placeholder="Όνομα χρήστη" onChange={e => setUsername(e.target.value)}></input>
-            <input className="user-form-field" placeholder="Κωδικός πρόσβασης" type="password" onChange={e => setPassword(e.target.value)}></input>
-            <button className="user-form-button" type="submit">Σύνδεση</button>
-          </form>
+          <p style={{ "marginBottom": "20px" }}>Εισάγετε τα στοιχεία σας για να συνεχίσετε</p>
+          <UserForm submit_button_text="Σύνδεση" onSubmit={handleLogin}></UserForm>
         </div>
       </div>
     </>
